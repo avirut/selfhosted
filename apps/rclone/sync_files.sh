c72b9dc0de3ce5a5cb112c72f4d075af6a819bf2
Avirut <avirut@kroac.net>
2024-05-12 13:13:36 -0500

    working rclone sync

--- apps/rclone/sync_files.sh
+++ apps/rclone/sync_files.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 rclone sync /data DEFAULT:${BUCKET_NAME} --progress --fast-list --rc-addr /tmp/rclone-backup-running
+
+wget -qO- ${UPTIME_KUMA_URL}/api/push/${UPTIME_KUMA_CODE}?status=up&msg=success&ping= &> /dev/null
